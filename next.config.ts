b58b8bb2a413ac0d67b0615e5320da8438153360
shivam <131395793+Shivam5538@users.noreply.github.com>
2026-08-26 18:06:33 +0530

    Remove deprecated eslint config key from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
-    ignoreBuildErrors: true, // Just to be extra safe for the deployment
+    ignoreBuildErrors: true,
   },
 };
 
